f3a1078a02166d848f068b766a5aebc96d8ec229
Ahmad Ayyaz <ahmada@tracersystems.co.uk>
2019-11-19 18:03:13 +0500

    Added SCA implementation for stripe

--- app/src/main/java/com/example/livedataandretrofit/MainActivity.java
+++ app/src/main/java/com/example/livedataandretrofit/MainActivity.java
@@ -14,28 +14,52 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.livedataandretrofit.adapters.BlogAdapter;
 import com.example.livedataandretrofit.models.Blog;
 import com.example.livedataandretrofit.viewmodels.BlogsViewModel;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.stripe.android.ApiResultCallback;
+import com.stripe.android.PaymentAuthConfig;
 import com.stripe.android.PaymentConfiguration;
 import com.stripe.android.PaymentIntentResult;
 import com.stripe.android.Stripe;
 import com.stripe.android.model.ConfirmPaymentIntentParams;
 import com.stripe.android.model.PaymentIntent;
+import com.stripe.android.model.PaymentMethod;
 import com.stripe.android.model.PaymentMethodCreateParams;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     SwipeRefreshLayout swipeRefresh;
     private BlogsViewModel mainViewModel;
-    Stripe stripe;
+    private static final String BACKEND_URL = "http://192.168.1.109:4242/";
+
+    private OkHttpClient httpClient = new OkHttpClient();
+    private String paymentIntentClientSecret;
+    private Stripe stripe;
     BlogAdapter mBlogAdapter;
 
     @Override
@@ -51,17 +75,110 @@ public class MainActivity extends AppCompatActivity {
             getPopularBlog();
         });
 
-        PaymentMethodCreateParams.Card card = PaymentMethodCreateParams.Card.create("tok_mastercard_debit_transferSuccess");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        String json = "{"
+                + "\"currency\":\"usd\","
+                + "\"items\":["
+                + "{\"id\":\"photo_subscription\"}"
+                + "]"
+                + "}";
+        RequestBody body = RequestBody.create(json, mediaType);
+        Request request = new Request.Builder()
+                .url(BACKEND_URL + "create-payment-intent")
+                .post(body)
+                .build();
+        httpClient.newCall(request)
+                .enqueue(new PayCallback(this));
+
+
+
+    }
+    private void onPaymentSuccess(@NonNull final Response response) throws IOException {
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, String>>(){}.getType();
+        Map<String, String> responseMap = gson.fromJson(
+                Objects.requireNonNull(response.body()).string(),
+                type
+        );
+
+        // The response from the server includes the Stripe publishable key and
+        // PaymentIntent details.
+        // For added security, our sample app gets the publishable key from the server
+        String stripePublishableKey = responseMap.get("publishableKey");
+        paymentIntentClientSecret = responseMap.get("clientSecret");
+
+
+        PaymentMethodCreateParams.Card card = new PaymentMethodCreateParams.Card.Builder()
+                .setNumber("4000000000003220")
+                .setCvc("322")
+                .setExpiryMonth(3)
+                .setExpiryYear(23).build();
         PaymentMethodCreateParams params = PaymentMethodCreateParams.create(card);
         if (params != null) {
             ConfirmPaymentIntentParams confirmParams = ConfirmPaymentIntentParams
-                    .createWithPaymentMethodCreateParams(params, "pi_1Db4Eu2eZvKYlo2Cet17VHlp_secret_IvrnS53GJker7rUaDH2sM8CRw");
-            stripe = new Stripe(getApplicationContext(), PaymentConfiguration.getInstance(getApplicationContext()).getPublishableKey());
+                    .createWithPaymentMethodCreateParams(params, paymentIntentClientSecret);
+            stripe = new Stripe(
+                    getApplicationContext(),
+                    Objects.requireNonNull(stripePublishableKey)
+            );
+            stripe.createPaymentMethod(params, new ApiResultCallback<PaymentMethod>() {
+                @Override
+                public void onSuccess(PaymentMethod paymentMethod) {
+                    String paymentMethodId = paymentMethod.id;
+                    String cardCountry = paymentMethod.card.country;
+                }
+
+                @Override
+                public void onError(@NotNull Exception e) {
+
+                }
+            });
             stripe.confirmPayment(this, confirmParams);
 
         }
+
     }
 
+    private static final class PayCallback implements Callback {
+        @NonNull private final WeakReference<MainActivity> activityRef;
+
+        PayCallback(@NonNull MainActivity activity) {
+            activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            final MainActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            activity.runOnUiThread(() ->
+                    Toast.makeText(
+                            activity, "Error: " + e.toString(), Toast.LENGTH_LONG
+                    ).show()
+            );
+        }
+
+        @Override
+        public void onResponse(@NonNull Call call, @NonNull final Response response)
+                throws IOException {
+            final MainActivity activity = activityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            if (!response.isSuccessful()) {
+                activity.runOnUiThread(() ->
+                        Toast.makeText(
+                                activity, "Error: " + response.toString(), Toast.LENGTH_LONG
+                        ).show()
+                );
+            } else {
+                activity.onPaymentSuccess(response);
+            }
+        }
+    }
     private void initializationViews() {
         swipeRefresh = findViewById(R.id.swiperefresh);
         mRecyclerView = findViewById(R.id.blogRecyclerView);
@@ -122,10 +239,12 @@ public class MainActivity extends AppCompatActivity {
 
 
             PaymentIntent paymentIntent = result.getIntent();
+            String paymentIntentId = paymentIntent.getId();
             PaymentIntent.Status status = paymentIntent.getStatus();
             if (status == PaymentIntent.Status.Succeeded) {
                 // Payment completed successfully
                 Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                String jsonObject = gson.toJson(paymentIntent);
 
             } else if (status == PaymentIntent.Status.RequiresPaymentMethod) {
                 // Payment failed
